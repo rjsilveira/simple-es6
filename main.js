a3ee5dfa380b8aebff7257541998eda79ea0ba08
Raphael Silveira <wid_rsilveira@uolinc.com>
2019-08-22 13:52:31 -0300

    Classes and Arrow functions

---
+++ main.js
@@ -0,0 +1,44 @@
+/**
+ * Classes
+ */
+class List {
+  constructor() {
+    this.data = [];
+  }
+
+  add(data) {
+    this.data.push(data);
+  }
+}
+
+class Task extends List {
+  retrieveAll() {
+    return this.data;
+  }
+}
+
+const task = new Task();
+task.add("Task 01");
+task.add("Task 02");
+task.add("Task 03");
+console.log("All tasks", task.retrieveAll());
+
+/**
+ * Arrays e Arrow functions
+ */
+const arr = [1, 5, 7, 9, 11, 14];
+
+const newArr = arr.map((item, index) => item * index);
+console.log(newArr);
+
+const sum = arr.reduce((total, next) => total + next);
+console.log(sum);
+
+const filter = arr.filter(item => item % 2 === 0);
+console.log(filter);
+
+const find = arr.find(item => item === 3);
+console.log(find);
+
+const teste = () => "teste";
+console.log(teste());
